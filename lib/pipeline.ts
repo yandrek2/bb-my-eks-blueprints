ad2be021185a1bcec3eba6cbfb291c766b57f638
EC2 Default User <ec2-user@ip-172-31-49-137.us-west-2.compute.internal>
2022-07-15 03:55:54 +0000

    Add AddOns

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -14,8 +14,28 @@ export default class PipelineConstruct extends Construct {
     const blueprint = blueprints.EksBlueprint.builder()
     .account(account)
     .region(region)
-    .addOns()
+    .addOns(new blueprints.ClusterAutoScalerAddOn, new blueprints.AwsLoadBalancerControllerAddOn())
     .teams(new TeamPlatform(account), new TeamApplication('Devs',account));
+  
+    const repoUrl = 'https://github.com/acastillo5690/capstone-project-apps';
+
+    const bootstrapRepo : blueprints.ApplicationRepository = {
+        repoUrl,
+        targetRevision: 'project',
+    }
+
+    const stagingBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'staging'
+        },
+    });
+    const productionBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'production'
+        },
+    });
   
     blueprints.CodePipelineStack.builder()
       .name("eks-blueprints-workshop-pipeline")
@@ -29,8 +49,8 @@ export default class PipelineConstruct extends Construct {
       .wave({
         id: "envs",
         stages: [
-          { id: "staging", stackBuilder: blueprint.clone('us-west-2')},
-          { id: "production", stackBuilder: blueprint.clone('us-east-1')}
+          { id: "staging", stackBuilder: blueprint.clone('us-west-2').addOns(stagingBootstrapArgo)},
+          { id: "production", stackBuilder: blueprint.clone('us-east-1').addOns(productionBootstrapArgo)}
         ]
       })
       
